7c67d9edd8108bdc7587731fd4acf816130cd4ca
Anaisl14 <laurentanais2@gmail.com>
2018-05-11 18:39:50 +0200

    upload

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -4,17 +4,12 @@ class CocktailsController < ApplicationController
     @cocktails = Cocktail.all
   end
 
-  def show
-    @cocktail = Cocktail.find(params[:id])
-  end
-
-
   def new
     @cocktail = Cocktail.new
   end
 
   def create
-    @cocktail = Cocktail.new( cocktail_params)
+    @cocktail = Cocktail.new(name: params["name"])
     if @cocktail.save
       redirect_to cocktail_path(@cocktail), notice:'Cocktail was successfully created'
     else
@@ -22,10 +17,16 @@ class CocktailsController < ApplicationController
     end
   end
 
-private
+  def show
+    @cocktail = Cocktail.find(params[:id])
+  end
 
-def cocktail_params
-  params.require(:cocktail).permit(:name)
-end
+
+
+  private
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
 
 end
